056687995cedf9bead771029a3b13455ef3411bd
EdgarGreenchuk <Edgar.greenchuk@gmail>
2026-01-19 14:05:29 +0200

    update CORS to allow all Netlify domains

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://poetic-gecko-739299.netlify.app'
+    /\.netlify\.app$/ // ← BET KOKS Netlify domain
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
